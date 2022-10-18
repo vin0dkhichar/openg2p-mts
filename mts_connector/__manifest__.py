b56f1c334f0d21a749ef3f7d2e1d0a5516cde2d3
Lalith Kota <kotalalith@gmail.com>
2022-10-18 22:15:07 +0530

    Added initial Implementations of mts_connector & g2p_mts

---
+++ mts_connector/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "MTS Connector",
+    "category": "MTS",
+    "version": "15.0.0.0.1",
+    "sequence": 1,
+    "author": "OpenG2P",
+    "website": "https://github.com/OpenG2P/openg2p-mts",
+    "license": "Other OSI approved licence",
+    "depends": ["base", "queue_job"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/security.xml",
+        "views/menuitems.xml",
+        "views/mts_connector.xml",
+    ],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
