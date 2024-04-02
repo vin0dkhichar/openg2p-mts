30613c779b5a32fa0cd59e85ef1186a8d312c321
Dibik <dibik@openg2p.org>
2024-04-02 21:37:17 +0530

    setting view  migrating to odoo17 and pre-commit error

--- g2p_mts/models/res_config_settings.py
+++ g2p_mts/models/res_config_settings.py
@@ -6,7 +6,7 @@ _logger = logging.getLogger(__name__)
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = ["res.config.settings"]
+    _inherit = "res.config.settings"
 
     g2p_mts_vid_delete_job_status = fields.Boolean(
         default=lambda x: x.env.ref("g2p_mts.to_delete_g2p_reg_id_vid").active
