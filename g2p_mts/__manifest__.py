9b0f3a3e528a4f6ef1c7d0bb3410608b20b1a8fc
Dibik <dibik@openg2p.org>
2024-04-02 22:05:49 +0530

    added test file with depends

--- g2p_mts/__manifest__.py
+++ g2p_mts/__manifest__.py
@@ -6,7 +6,7 @@
     "website": "https://openg2p.org",
     "license": "Other OSI approved licence",
     "development_status": "Alpha",
-    "depends": ["mts_connector"],
+    "depends": ["mts_connector", "g2p_registry_base"],
     "data": [
         "data/cron.xml",
         "views/g2p_mts_connector.xml",
